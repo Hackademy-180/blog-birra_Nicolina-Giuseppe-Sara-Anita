b54b972a47cfb7c142bf203df633a167c222b37b
Nicolina <nicolina.vesco@gmail.com>
2026-01-28 09:49:45 +0100

    Caricate birre nuove

--- resources/views/beers/detail.blade.php
+++ resources/views/beers/detail.blade.php
@@ -1,29 +1,26 @@
 <x-layout>
-    <h1>Dettaglio prodotto:</h1>
+    <h1 class="text-center mt-5">Dettaglio prodotto:</h1>
     <main class="container">
-        <section class="row">
+        <section class="row justify-content-center mt-5">
             <article class="col-12 col-md-6 text-center">
                 <p class="lead">
-                    <strong>Nome:</strong>
+                    <strong class="fw-bold">Nome: </strong>{{$beer->name}}
                 </p>
                 <p class="lead">
-                    <strong>Birrificio:</strong>
+                    <strong class="fw-bold">Birrificio: </strong>{{$beer->brewery}}
                 </p>
                 <p class="lead">
-                    <strong>Stile:</strong>
+                    <strong class="fw-bold">Stile: </strong>{{$beer->style}}
                 </p>
                 <p class="lead">
-                    <strong>Descrizione:</strong>
+                    <strong class="fw-bold">Descrizione: </strong>{{$beer->info}}
                 </p>
                 <p class="lead">
-                    <strong>Immagine:</strong>
-                </p>
-                <p class="lead">
-                    <strong>Creato da:</strong>
+                    <strong class="fw-bold">Creato da: </strong>
                 </p>
                 @if(Auth::id()== $beer->user->id)
-                <a href="" class="btn btn-warning">Modifica</a>
-                <a href="" class="btn btn-warning">Tutte le birre</a>
+                <a href="{{route('beer_edit')}}" class="btn btn-warning">Modifica</a>
+                <a href="{{route('beer_index')}}" class="btn btn-warning">Tutte le birre</a>
                 @endif
             </article>
         </section>
